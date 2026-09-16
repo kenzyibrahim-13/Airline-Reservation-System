c743bbab5ccb471f10dc6e1e3bb0d4335c8efd55
kenzyibrahim-13 <kenzyibrahim97@gmail.com>
2026-09-16 23:09:02 +0300

    Remove DemoVid.mp4

---
+++ cpp_src/build/CMakeFiles/airline_bridge.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "C:/Users/11/Documents/OOP_Project/final_proj/windows/runner/libairline_bridge.dll"
+  "C:/Users/11/Documents/OOP_Project/final_proj/windows/runner/libairline_bridge.dll.manifest"
+  "C:/Users/11/Documents/OOP_Project/final_proj/windows/runner/libairline_bridge.pdb"
+  "CMakeFiles/airline_bridge.dir/Admin.cpp.obj"
+  "CMakeFiles/airline_bridge.dir/Admin.cpp.obj.d"
+  "CMakeFiles/airline_bridge.dir/AuthSystem.cpp.obj"
+  "CMakeFiles/airline_bridge.dir/AuthSystem.cpp.obj.d"
+  "CMakeFiles/airline_bridge.dir/FileManager.cpp.obj"
+  "CMakeFiles/airline_bridge.dir/FileManager.cpp.obj.d"
+  "CMakeFiles/airline_bridge.dir/Flight.cpp.obj"
+  "CMakeFiles/airline_bridge.dir/Flight.cpp.obj.d"
+  "CMakeFiles/airline_bridge.dir/Luggage.cpp.obj"
+  "CMakeFiles/airline_bridge.dir/Luggage.cpp.obj.d"
+  "CMakeFiles/airline_bridge.dir/Passenger.cpp.obj"
+  "CMakeFiles/airline_bridge.dir/Passenger.cpp.obj.d"
+  "CMakeFiles/airline_bridge.dir/SuperAdmin.cpp.obj"
+  "CMakeFiles/airline_bridge.dir/SuperAdmin.cpp.obj.d"
+  "CMakeFiles/airline_bridge.dir/SystemManager.cpp.obj"
+  "CMakeFiles/airline_bridge.dir/SystemManager.cpp.obj.d"
+  "CMakeFiles/airline_bridge.dir/Ticket.cpp.obj"
+  "CMakeFiles/airline_bridge.dir/Ticket.cpp.obj.d"
+  "CMakeFiles/airline_bridge.dir/User.cpp.obj"
+  "CMakeFiles/airline_bridge.dir/User.cpp.obj.d"
+  "CMakeFiles/airline_bridge.dir/bridge.cpp.obj"
+  "CMakeFiles/airline_bridge.dir/bridge.cpp.obj.d"
+  "libairline_bridge.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/airline_bridge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp_src/build/CMakeFiles/airline_bridge.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for airline_bridge.
+# This may be replaced when dependencies are built.
